4bf50de1f9877cc7db1a446c208ef559363d0ef3
QC <iostreamplus@proton.me>
2026-04-16 22:41:28 +0000

    o

--- cogs/general.py
+++ cogs/general.py
@@ -10,7 +10,7 @@ class General(commands.Cog):
         embed = discord.Embed(
             title="🏓 Pong!",
             description=f"Latency: {round(self.bot.latency * 1000)}ms",
-            color=0xBEBEFE,
+            color=0xE62055,
         )
         await ctx.send(embed=embed)
 
@@ -18,6 +18,26 @@ class General(commands.Cog):
     async def hi(self, ctx):
         await ctx.send(f"Hello {ctx.author.name}!")
 
+
+    @commands.command()
+    async def lb(self, ctx):
+        guild_id = ctx.guild.id
+
+        if guild_id is None:
+            embed = discord.Embed(
+                title="No leaderboard available",
+                description=f"You must be in a server",
+                color=0xE5AC12,
+            )
+        else:
+            embed = discord.Embed(
+                title="Server Leaderboard",
+                description=f"Top 10 identifiers",
+                color=0xE5AC12,
+            )
+            leaderboard = self.bot.db.get_leaderboard(guild_id)
+            await ctx.send(leaderboard)
+        await ctx.send(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(General(bot))
